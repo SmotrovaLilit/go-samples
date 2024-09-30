6a50f94be682abb354a944281f09d5c7a318b6ec
Lilit Smotrova <l.smotrova@omp.ru>
2024-09-30 21:17:59 +0200

    Init blot

---
+++ blot/cmd/blot/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"gosamples/blot/internal/domain/player"
+	"gosamples/blot/internal/domain/team"
+)
+
+func main() {
+	player1 := player.NewPlayer("Alice")
+	player2 := player.NewPlayer("Bob")
+	player3 := player.NewPlayer("Charlie")
+	player4 := player.NewPlayer("David")
+
+	team1 := team.NewTeam("Team 1", player1, player2)
+	team2 := team.NewTeam("Team 2", player3, player4)
+	players := []player.Player{player1, player2, player3, player4}
+
+}
